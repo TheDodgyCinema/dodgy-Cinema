f766766aa96d9add5492b0522babf97dabb980e7
Chad Stainbank <chad.stainbank@gmail.com>
2018-05-17 11:49:35 +0100

    Implement hello world service functionality
    This greets everyone in the database

---
+++ src/main/java/com/qa/dodgy/business/service/HelloWorldService.java
@@ -0,0 +1,28 @@
+package com.qa.dodgy.business.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import com.qa.dodgy.business.repository.IHelloWorldRepository;
+import com.qa.dodgy.persistence.domain.HelloWorld;
+
+public class HelloWorldService implements IHelloWorldService {
+	@Inject
+	IHelloWorldRepository repo;
+
+	public String sayHello() {
+		String names = String.join(", ", getNames());
+		return String.format("{\"message\": \"Hello my dodgy friends!\", \"friends\": \"%s\"}", names);
+	}
+	
+	private List<String> getNames() {
+		List<HelloWorld> helloWorlds = repo.getHelloWorlds();
+		List<String> names = new ArrayList<String>();
+		for (HelloWorld helloWorld : helloWorlds) {
+			names.add(helloWorld.getName());
+		}
+	return names;
+	}
+}
--- src/main/java/com/qa/dodgy/interoperability/TestEndpoint.java
+++ src/main/java/com/qa/dodgy/interoperability/TestEndpoint.java
@@ -1,15 +1,21 @@
 package com.qa.dodgy.interoperability;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import com.qa.dodgy.business.service.IHelloWorldService;
+
 @Path("test")
 public class TestEndpoint {
+	@Inject
+	private IHelloWorldService service;
+
 	@GET
 	@Produces("application/json")
 	public String sayHello() {
-		return "{\"message\": \"Hello my dodgy friend!\")";
+		return service.sayHello();
 	}
 
 }
