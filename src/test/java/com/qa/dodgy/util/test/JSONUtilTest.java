5fa6ba6cd178765dc3e99ec86c629a7675fe10d8
Chad Stainbank <chad.stainbank@gmail.com>
2018-05-18 13:15:54 +0100

    Change unit test style according to Codacy recommendations

--- src/test/java/com/qa/dodgy/util/test/JSONUtilTest.java
+++ src/test/java/com/qa/dodgy/util/test/JSONUtilTest.java
@@ -11,15 +11,15 @@ import com.qa.dodgy.util.JSONUtil;
 
 public class JSONUtilTest {
 	
+	private static final String MOCK_OBJECT = "{\"title\":\"IT\",\"rating\":\"18\",\"subtitles\":\"English\",\"releaseYear\":\"2017\",\"director\":\"Andy Muschietti\",\"actors\":\"Jaeden Lieberher\",\"reviews\":\"Good\"}";
+
 	private JSONUtil util;
 	
 	@Before
-	public void setup() {
+	public void setUp() {
 		util = new JSONUtil();
 	}
 	
-	private static final String MOCK_OBJECT = "{\"title\":\"IT\",\"rating\":\"18\",\"subtitles\":\"English\",\"releaseYear\":\"2017\",\"director\":\"Andy Muschietti\",\"actors\":\"Jaeden Lieberher\",\"reviews\":\"Good\"}";
-
 	@Test
 	public void movieConversionToJSONTest() {
 		Movie movie = new Movie("IT", "18", "English", "2017", "Andy Muschietti", "Jaeden Lieberher", "Good");
