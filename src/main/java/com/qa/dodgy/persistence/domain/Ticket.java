1d69e6577e91e774bfe754471a2a8cf71ebdd7f9
jhart7614 <j.hart7614@gmail.com>
2018-05-21 17:27:48 +0100

    Remove Ticket entity getters and setters
    The Ticket entity doesn't need getters, setters or an argument
    constructor yet, so these have been removed to simplify the
    code until they are shown to be required for ticket creation.

--- src/main/java/com/qa/dodgy/persistence/domain/Ticket.java
+++ src/main/java/com/qa/dodgy/persistence/domain/Ticket.java
@@ -21,26 +21,6 @@ public class Ticket {
 	public Ticket() {
 		super();
 	}
-	
-	public Long getId() {
-		return id;
-	}
-
-	public Showing getShowing() {
-		return showing;
-	}
-
-	public void setShowing(Showing showing) {
-		this.showing = showing;
-	}
-
-	public Seat getSeat() {
-		return seat;
-	}
-
-	public void setSeat(Seat seat) {
-		this.seat = seat;
-	}
 
 }
 
