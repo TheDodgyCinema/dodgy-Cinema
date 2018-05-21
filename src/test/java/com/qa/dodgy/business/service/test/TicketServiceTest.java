90e1214191af154d02db597d24ab6513ad72ef31
jhart7614 <j.hart7614@gmail.com>
2018-05-21 17:37:14 +0100

    Write test for get ticket functionality

---
+++ src/test/java/com/qa/dodgy/business/service/test/TicketServiceTest.java
@@ -0,0 +1,45 @@
+package com.qa.dodgy.business.service.test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.util.HashMap;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.qa.dodgy.business.repository.TicketRepositoryDB;
+import com.qa.dodgy.business.service.TicketService;
+import com.qa.dodgy.persistence.domain.Ticket;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TicketServiceTest {
+
+	@InjectMocks
+	private TicketService service = new TicketService();
+	
+	@Mock
+	private TicketRepositoryDB repo = new TicketRepositoryDB();
+	
+	private HashMap<Long, Ticket> ticketDB = new HashMap<Long, Ticket>();
+	
+	@Before
+	public void setUp() {
+		service.setRepo(repo);
+		ticketDB.put(0L, new Ticket());
+		ticketDB.put(1L, new Ticket());
+	}
+
+	@Test
+	public void getTicketTest() {
+		long id = 0;
+		Ticket ticket = ticketDB.get(id);
+		when(repo.getTicket(id)).thenReturn(ticket);
+		assertEquals(ticketDB.get(id), service.getTicket(id));
+	}
+
+}
