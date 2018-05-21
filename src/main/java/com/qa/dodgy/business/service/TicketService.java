707ef4b71fde125a7ff1039fdf7006d414417187
jhart7614 <j.hart7614@gmail.com>
2018-05-21 16:02:41 +0100

    updated get ticket

--- src/main/java/com/qa/dodgy/business/service/TicketService.java
+++ src/main/java/com/qa/dodgy/business/service/TicketService.java
@@ -5,13 +5,18 @@ import javax.inject.Inject;
 import com.qa.dodgy.business.repository.ITicketRepository;
 import com.qa.dodgy.persistence.domain.Ticket;
 
-public class TicketService {
+public class TicketService implements ITicketService {
 	
 	@Inject
 	private ITicketRepository repo;
 	
 	public Ticket getTicket(Long id) {
 		return repo.getTicket(id);
+	}
+
+	@Override
+	public void setRepo(ITicketRepository repo) {
+		this.repo = repo;	
 	}
 
 }
