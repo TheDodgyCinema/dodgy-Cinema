4d52f510b1f783f2c076f985c80c8b59a6ba26ff
jhart7614 <j.hart7614@gmail.com>
2018-05-22 09:40:13 +0100

    Added test for get screen

---
+++ src/test/java/com/qa/dodgy/business/service/test/ScreenServiceTest.java
@@ -0,0 +1,46 @@
+package com.qa.dodgy.business.service.test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.qa.dodgy.business.repository.ScreenRepositoryDB;
+import com.qa.dodgy.business.service.ScreenService;
+import com.qa.dodgy.persistence.domain.Movie;
+import com.qa.dodgy.persistence.domain.Screen;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ScreenServiceTest {
+
+	@InjectMocks
+	private ScreenService service = new ScreenService();
+	
+	@Mock
+	private ScrenRepositoryDB repo = new ScreenRepositoryDB();
+	
+	private HashMap<Long, Screen> screenDB = new HashMap<Long, Screen>();
+	
+	@Before
+	public void setUp() {
+		service.setRepo(repo);
+		screenDB.put(0L, new Screen("3", "15", "Imax"));
+		screenDB.put(1L, new Screen("2", "18", "Standard"));
+	}
+	
+	@Test
+	public void getScreenTest() {
+		long id = 0;
+		Movie screen = screenDB.get(id);
+		when(repo.getScreen(id)).thenReturn(screen);
+		assertEquals(screenDB.get(id), service.getScreen(id));
+	}
+
+}
